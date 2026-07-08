7afbf67d2aea715d1e25ab4e90b419a5567164c9
Kavin Maziya <kavinmaziya256@gmail.com>
2026-07-08 07:31:50 +0200

    feat: Part 4 - Wire HomeScreen with four job variants and Material 3 theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,93 @@
+// lib/main.dart
+
 import 'package:flutter/material.dart';
+import 'package:careerhub_mobile/models/job.dart';
+import 'package:careerhub_mobile/widgets/job_card.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CareerHubApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CareerHubApp extends StatelessWidget {
+  const CareerHubApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CareerHub',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF0D3B36), // custom deep teal
+          brightness: Brightness.light,
+        ),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  static final List<Job> _jobs = [
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+    //Job 1 - Fully populated, open job.
+    Job(
+      title: 'Senior Frontend Software Engineer',
+      company: 'TechCorp Cape Town',
+      location: 'Cape Town',
+      description: 'We are looking for a talented Senior Frontend Engineer...',
+      employmentType: 'Full-Time',
+      isOpen: true,
+      salary: 37500,
+      closingDate: DateTime(2026, 7, 24),
+    ),
 
-  final String title;
+    // Job 2 - Open job, no salary disclosed, no closing date.
+    Job(
+      title: 'UX/Web Designer',
+      company: 'DesignHouse Sandton',
+      location: 'Sandton',
+      description: 'We are looking for a creative UX/Web Designer...',
+      employmentType: 'Contract',
+      isOpen: true,
+    ),
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+    // Job 3 - Closed job via named constructor.
+    Job.closed(
+      title: 'Data Analyst Intern',
+      company: 'DataWorks Pretoria',
+      location: 'Pretoria/Hybrid',
+      description: 'We are looking for a Data Analyst Intern...',
+      employmentType: 'Internship',
+      salary: 18500,
+      closingDate: DateTime(2026, 6, 19),
+    ),
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+    // Job 4 - Remote job via named constructor.
+    Job.remote(
+      title: 'Part-Time Content Writer/Promoter',
+      company: 'MediaCo',
+      description: 'We are looking for a Content Writer...',
+      employmentType: 'Part-Time',
+      isOpen: true,
+      salary: 15000,
+      closingDate: DateTime(2026, 7, 24),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('CareerHub'),
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: ListView.builder(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        itemCount: _jobs.length,
+        itemBuilder: (context, index) => JobCard(job: _jobs[index]),
       ),
     );
   }
