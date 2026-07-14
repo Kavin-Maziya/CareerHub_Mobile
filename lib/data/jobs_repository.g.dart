f84496ea922bdf0ec42ed9cdfa3ae10f8c139bac
Kavin Maziya <kavinmaziya256@gmail.com>
2026-07-14 23:52:08 +0200

    feat: Part 4 - Add JobsNotifier and run code generation

---
+++ lib/data/jobs_repository.g.dart
@@ -0,0 +1,91 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'jobs_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(dio)
+final dioProvider = DioProvider._();
+
+final class DioProvider extends $FunctionalProvider<Dio, Dio, Dio>
+    with $Provider<Dio> {
+  DioProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'dioProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$dioHash();
+
+  @$internal
+  @override
+  $ProviderElement<Dio> $createElement($ProviderPointer pointer) =>
+      $ProviderElement(pointer);
+
+  @override
+  Dio create(Ref ref) {
+    return dio(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(Dio value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<Dio>(value),
+    );
+  }
+}
+
+String _$dioHash() => r'57cba5cb958b3d58ab3882e976a91690d80449d8';
+
+@ProviderFor(jobsRepository)
+final jobsRepositoryProvider = JobsRepositoryProvider._();
+
+final class JobsRepositoryProvider
+    extends $FunctionalProvider<JobsRepository, JobsRepository, JobsRepository>
+    with $Provider<JobsRepository> {
+  JobsRepositoryProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'jobsRepositoryProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$jobsRepositoryHash();
+
+  @$internal
+  @override
+  $ProviderElement<JobsRepository> $createElement($ProviderPointer pointer) =>
+      $ProviderElement(pointer);
+
+  @override
+  JobsRepository create(Ref ref) {
+    return jobsRepository(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(JobsRepository value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<JobsRepository>(value),
+    );
+  }
+}
+
+String _$jobsRepositoryHash() => r'697d55ff7c33af66151c331a0ad73f030a41944e';
---
+++ lib/providers/jobs_notifier.g.dart
@@ -0,0 +1,54 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'jobs_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(JobsNotifier)
+final jobsProvider = JobsNotifierProvider._();
+
+final class JobsNotifierProvider
+    extends $AsyncNotifierProvider<JobsNotifier, List<Job>> {
+  JobsNotifierProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'jobsProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$jobsNotifierHash();
+
+  @$internal
+  @override
+  JobsNotifier create() => JobsNotifier();
+}
+
+String _$jobsNotifierHash() => r'9d3ab68a03646c511bea354468197a29e34bd863';
+
+abstract class _$JobsNotifier extends $AsyncNotifier<List<Job>> {
+  FutureOr<List<Job>> build();
+  @$mustCallSuper
+  @override
+  WhenComplete runBuild() {
+    final ref = this.ref as $Ref<AsyncValue<List<Job>>, List<Job>>;
+    final element =
+        ref.element
+            as $ClassProviderElement<
+              AnyNotifier<AsyncValue<List<Job>>, List<Job>>,
+              AsyncValue<List<Job>>,
+              Object?,
+              Object?
+            >;
+    return element.handleCreate(ref, build);
+  }
+}
