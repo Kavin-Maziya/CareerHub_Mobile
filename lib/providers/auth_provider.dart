8435e0570e01894ab639fb65285ae29382075f62
Kavin Maziya <kavinmaziya256@gmail.com>
2026-07-22 01:31:17 +0200

    Part 6 - Auth Provider and Router Bridge + added authentication state listener and unauthenticated callback

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'auth_notifier.dart';
+
+
+final onUnauthenticatedProvider =
+    Provider<void Function()>((ref) {
+
+  return () {
+    ref.invalidate(authNotifierProvider);
+  };
+});
+
+
+class AuthStateListenable extends ChangeNotifier {
+
+  AuthStateListenable(this.ref) {
+    _subscription = ref.listen(
+      authNotifierProvider,
+      (_, _) {
+        notifyListeners();
+      },
+    );
+  }
+
+
+  final Ref ref;
+
+  late final ProviderSubscription _subscription;
+
+
+  @override
+  void dispose() {
+    _subscription.close();
+    super.dispose();
+  }
+}
+
+
+final authStateListenableProvider =
+    Provider<AuthStateListenable>((ref) {
+
+  final listenable = AuthStateListenable(ref);
+
+  ref.onDispose(
+    listenable.dispose,
+  );
+
+  return listenable;
+});
